6d1cebceaabeac2df0d76caf9afbb8002eb1d5c4
Wilkin <wilkinn@buildpulse.com>
2018-08-12 12:52:26 -0700

    log unmatched header

--- src/models/request.js
+++ src/models/request.js
@@ -1,34 +1,46 @@
 'use strict';
 
 var _ = require('underscore'),
-  helpers = require('../helpers/request.helpers.js');
+    helpers = require('../helpers/request.helpers.js');
 
 function Request(options) {
-  this.method = options.method && options.method.toLowerCase();
-  this.path = options.path;
-  this.query = options.query || '';
-  this.headers = options.headers || {};
-  this.body = options.body || {};
+    this.method = options.method && options.method.toLowerCase();
+    this.path = options.path;
+    this.query = options.query || '';
+    this.headers = options.headers || {};
+    this.body = options.body || {};
 
-  helpers.makeHeaderNamesLowerCaseRemoveSpaces(this.headers);
+    helpers.makeHeaderNamesLowerCaseRemoveSpaces(this.headers);
 }
 
 Request.prototype.match = function (request) {
-  if (request.constructor !== Request) {
-    request = new Request(request);
-  }
+    if (request.constructor !== Request) {
+        request = new Request(request);
+    }
 
-  var isMethodTheSame = request.method && request.method.toLowerCase();
-  isMethodTheSame = _.isEqual(this.method, isMethodTheSame);
+    var isMethodTheSame = request.method && request.method.toLowerCase();
+    isMethodTheSame = _.isEqual(this.method, isMethodTheSame);
 
-  var expQuery = decodeURIComponent(helpers.parseQueryParams(this.query)),
-    reqQuery = decodeURIComponent(helpers.parseQueryParams(request.query));
+    var expQuery = decodeURIComponent(helpers.parseQueryParams(this.query)),
+        reqQuery = decodeURIComponent(helpers.parseQueryParams(request.query));
 
-  return isMethodTheSame &&
-    _.isEqual(this.path, request.path) &&
-    _.isEqual(expQuery, reqQuery) &&
-    helpers.areAllExpectationHeadersPesentInRequest(this.headers, request.headers) &&
-    _.isEqual(this.body, request.body);
+    var headerSame = helpers.areAllExpectationHeadersPesentInRequest(this.headers, request.headers);
+    var querySame = _.isEqual(expQuery, reqQuery);
+    var bodySame = _.isEqual(this.body, request.body);
+    var pathSame = _.isEqual(this.path, request.path);
+    var ret = pathSame && isMethodTheSame && querySame && bodySame && headerSame;
+
+    if (!ret && pathSame && isMethodTheSame && querySame) {
+        console.log("*** Error matching ", request.path);
+        if (!headerSame) {
+            console.log("header:\n", request.headers, "\n expecting:\n", this.headers);
+        }
+        if (!bodySame) {
+            console.log("body:\n", request.body, "\n expecting:\n", this.body);
+        }
+    }
+    return ret
+
 };
 
 module.exports = Request;
